ff68e836541dc1ed4bdb4eefa6c0b6b0b5da87de
tryam <tryamkin@gmail.com>
2024-02-21 17:44:08 +0200

    simple

--- src/test/java/core/BaseSeleniumTest.java
+++ src/test/java/core/BaseSeleniumTest.java
@@ -1,9 +1,14 @@
 package core;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.itfriendly.core.BaseSeleniumPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -19,32 +24,33 @@ abstract public class BaseSeleniumTest {
     protected WebDriver driver;
 
 
-    @BeforeClass
-    public void setUp() {
+
+    //    @BeforeClass
+//    public void setUp() {
+
+//
+//        if (OS_NAME_FOR_GIT.equals("Linux")) {
+//            driver = gitRunConfig(driver, BROWSER_NAME);
+//        } else {
+//            driver = chooseDriver(driver, BROWSER_NAME);
+//        }
+//
+//        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(PAGELOAD_WAIT));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLISITY_WAIT));
+//        BaseSeleniumPage.setDriver(driver);
+//
+//    }
 
 
-        if (OS_NAME_FOR_GIT.equals("Linux")) {
-            driver = gitRunConfig(driver, BROWSER_NAME);
-        } else {
-            driver = chooseDriver(driver, BROWSER_NAME);
-        }
+//    @AfterClass
+//    public void tearDown() {
+//        switch (BROWSER_NAME) {
+//            case "CHROME" -> {
+//                driver.close();
+//                driver.quit();
+//            }
+//            case "FIREFOX" -> driver.quit();
+//        }
+//    }
 
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(PAGELOAD_WAIT));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLISITY_WAIT));
-        BaseSeleniumPage.setDriver(driver);
-
-    }
-
-
-
-    @AfterClass
-    public void tearDown() {
-        switch (BROWSER_NAME) {
-            case "CHROME" -> {
-                driver.close();
-                driver.quit();
-            }
-            case "FIREFOX" -> driver.quit();
-        }
-    }
 }
